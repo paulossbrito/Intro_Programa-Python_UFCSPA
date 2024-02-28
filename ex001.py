87e0756eead2b1d965ff4eb9c8fdaf84241fbad0
Paulo Brito <130158405+paulossbrito@users.noreply.github.com>
2024-02-28 09:37:07 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    saud1 = "Olá"
+    saud2 = "Seja bem vindo!"
+    
+    print(f"{saud1}\n{saud2}")
+
+
+
+
+
+#Escreva em Python um programa que armazene os valores “Olá,” e “Seja bem vindo” em variáveis separadas e
+#depois exiba-os, uma abaixa da outra.
---
+++ ex002.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    n1 = 10
+    n2 = 5
+    soma = n1 + n2
+    print(f"Soma = {soma}")
+
+
+
+
+
+#Escreva em Python um programa que armazene dois valores numéricos em variáveis separadas e depois exiba a
+#soma delas.
